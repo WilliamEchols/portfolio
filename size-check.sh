0130574f008af1ecf8531fd13404bd56ca28c069
William Echols <echolswd@gmail.com>
2025-06-16 16:47:24 -0500

    number crunching restructure

--- size-check.sh
+++ size-check.sh
@@ -10,7 +10,7 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 # Files to include in calculation
-FILES=("index.html" "blog/index.html" "fractals/index.html" "log2/index.html")
+FILES=("index.html" "blog/index.html" "fractals/index.html" "log2/index.html" "gamma1d5/index.html" "brainf/index.html" "reu/index.html" "number-crunching/index.html")
 
 total_bytes=0
 
